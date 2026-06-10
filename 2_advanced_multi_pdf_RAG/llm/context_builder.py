19dc9ac706e9a6e86c58b1520ca4ccdcd67ffe57
preethamepuri <preethamepuri@gmail.com>
2026-06-10 18:12:00 +0530

    Remove 2_advanced_multi_pdf_RAG from repository

--- 2_advanced_multi_pdf_RAG/llm/context_builder.py
+++
@@ -1,6 +0,0 @@
-def build_context(retrieved_chunks):
-    context = "\n\n".join(
-        chunk["text"]
-        for chunk in retrieved_chunks
-    )
-    return context
--- 2_advanced_multi_pdf_RAG/retrieval/embedder.py
+++
@@ -1,7 +0,0 @@
-from sentence_transformers import SentenceTransformer
-
-model = SentenceTransformer("nomic-ai/nomic-embed-text-v1.5", trust_remote_code=True) # --> a better pretrained embedding model
-
-def create_embeddings(chunks):
-    texts = [chunk["text"] for chunk in chunks]
-    return model.encode(texts)
